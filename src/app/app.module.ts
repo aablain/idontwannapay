7a8c4ddfcf4802d842fa8f9716a46cad6d377c09
Austin Blain and Joshua Rinard <austin@>
2017-01-26 09:41:52 -0800

    all routes and finish setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { routing } from './app.routing';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ProjectsListComponent } from './projects-list/projects-list.component';
 import { SpecificProjectComponent } from './specific-project/specific-project.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     ProjectsListComponent,
-    SpecificProjectComponent
+    SpecificProjectComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
